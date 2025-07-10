66e7053f88f3627656e09b3de240a2c7946834e8
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-07-10 07:55:56 -0500

    Cambios realizados, se creo el folder de VIewModels y se modificacion los geters y seters dentro de nuestra alace PeliculasViewModel.cs.

--- ExamenProgreso3/ViewModels/PeliculasVIewModel.cs
+++ ExamenProgreso3/ViewModels/PeliculasVIewModel.cs
@@ -9,6 +9,60 @@ namespace ExamenProgreso3.ViewModels
 {
     internal class PeliculasVIewModel: INotifyPropertyChanged
     {
+        private string _titulo;
+        private string _genero;
+        private int _anio;
+        private int _calificacion;  
+        public string Titulo
+        {
+            get => _titulo;
+            set
+            {
+                _titulo = value;
+                OnPropertyChanged(nameof(Titulo));
+            }
+        }
+        public string Genero
+        {
+            get => _genero;
+            set
+            {
+                _genero = value;
+                OnPropertyChanged(nameof(Genero));
+            }
+        }
+        public int Anio
+        {
+            get => _anio;
+            set
+            {
+                if (_anio < 2024) 
+                { // Validación para que el año no sea mayor a 2024
+                    throw new ArgumentOutOfRangeException(nameof(Anio), "El año no puede ser mayor a 2024.");
+                }
+                _anio = value;
+                OnPropertyChanged(nameof(Anio));
+            }
+        }
+
+        public int Calificacion 
+        {
+            get => _calificacion;
+            set 
+            {
+                if (_calificacion < 3) 
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Calificacion), "La calificación no puede ser menor a 3.");
+                }
+                _calificacion = value;
+                OnPropertyChanged(nameof(Calificacion));
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
-    }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+    }   
 }
---
+++ ExamenProgreso3/Views/Pelicias.xaml.cs
@@ -0,0 +1,10 @@
+namespace ExamenProgreso3.Views;
+
+public partial class Pelicias : ContentPage
+{
+	public Pelicias()
+	{
+		InitializeComponent();
+		//BindingContext = new ViewModels.PeliculasVIewModel();
+    }
+}
